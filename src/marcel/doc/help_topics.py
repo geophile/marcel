77fa77f005d6e18250631adc721460ada11bcc3b
Jack Orenstein <jao@geophile.com>
2020-04-25 22:06:22 -0400

    Missed a file.

---
+++ src/marcel/doc/help_topics.py
@@ -0,0 +1,33 @@
+HELP = '''
+Here is the complete list of help topics. Topic names are case-insensitive.
+
+b{Top-level help:}
+
+    - {marcel} (or just run {help} with no topic)
+
+b{Overview:}
+   
+    - {overview}
+    - {objects}
+    - {command}
+    - {function}
+    - {pipeline}
+
+b{Objects}:
+
+    - {file}
+    - {process}
+
+b{Operators:}
+
+    - {bash}        - {bg}          - {cd}
+    - {dirs}        - {edit}        - {expand}
+    - {fg}          - {gen}         - {head}
+    - {help}        - {jobs}        - {ls}
+    - {map}         - {out}         - {popd}
+    - {ps}          - {pushd}       - {pwd}
+    - {red}         - {reverse}     - {select}
+    - {sort}        - {squish}      - {sudo}
+    - {tail}        - {timer}       - {unique}
+    - {version}     - {window}
+'''
