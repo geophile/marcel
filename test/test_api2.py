1225fd0bbb23871dc41bfec2f417d810f9560be6
Jack Orenstein <jao@geophile.com>
2020-07-24 21:49:26 -0400

    Re-enabled tests.

--- test/test_api2.py
+++ test/test_api2.py
@@ -1,7 +1,3 @@
 from marcel.api import *
 
-even = []
-odd = []
-run(gen(10) | ifelse(lambda x: x % 2 == 0, store(even)) | store(odd))
-run(load(even))
-run(load(odd))
+run(ls('~/zack_disk*') | read())
