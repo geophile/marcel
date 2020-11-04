2e636fcff3c645bc1cec5501669fb26a7f1dbd81
Jack Orenstein <jao@geophile.com>
2020-11-04 12:24:17 -0500

    Added --command arg to history op.

--- marcel/op/history.py
+++ marcel/op/history.py
@@ -19,13 +19,22 @@ import marcel.object.historyrecord
 
 
 HELP = '''
-{L,wrap=F}history [N]
+{L,wrap=F}history [-c|--command STRING] [N]
+
+{L,indent=4:28}{r:-c}, {r:--command}           Report only the commands containing the specified STRING.
+
+{L,indent=4:28}{r:N}                       Include the last N qualifying items.
 
 Generates a stream containing the history of commands executed, in chronological order (newest last).
 The number identifying each command can be used in conjunction with the {r:edit}, {r:run}, 
 and {r:!} operators.
 
-If {r:N} is provided, it must be a positive integer. The most recent {r:N} commands will be output.
+If {r:--command} is specified, then only the commands containing the specified {r:STRING} will be
+selected. 
+
+If {r:N} is provided, it must be a positive integer. The most recent {r:N} qualifying commands will be output.
+(I.e., If {r:--command} is specified, than that filter is applied first, and then the last {r:N} items
+are selected.)
 '''
 
 
@@ -37,6 +46,7 @@ class HistoryArgsParser(marcel.argsparser.ArgsParser):
 
     def __init__(self, env):
         super().__init__('history', env)
+        self.add_flag_one_value('command', '-c', '--command')
         self.add_anon('n', convert=self.str_to_int, default=None)
         self.validate()
 
@@ -45,18 +55,31 @@ class History(marcel.core.Op):
 
     def __init__(self, env):
         super().__init__(env)
+        self.command = None
         self.n = None
 
     def __repr__(self):
-        return 'history()' if self.n is None else f'history({self.n})'
+        args = []
+        if self.command:
+            args.append(f"command='{self.command}'")
+        if self.n is not None:
+            args.append(f'n={self.n}')
+        args_description = '\n'.join(args)
+        return f'history({args_description})'
 
     # AbstractOp
 
     def receive(self, _):
         history = self.env().reader.history()
-        start = 0 if self.n is None else len(history) - self.n
-        for i in range(start, len(history)):
-            self.send(marcel.object.historyrecord.HistoryRecord(i, history[i]))
+        selected = []
+        for i in range(len(history)):
+            if self.command is None or self.command in history[i]:
+                selected.append(marcel.object.historyrecord.HistoryRecord(i, history[i]))
+        output = (selected
+                  if self.n is None else
+                  selected[-self.n:])
+        for record in output:
+            self.send(record)
 
     # Op
 
